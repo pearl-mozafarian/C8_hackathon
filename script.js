73762039e931be1cdf1ae8ac951787e6d3455615
morvarid mozafarian <morvarid mozafarian>
2016-05-12 11:18:03 -0700

    started js sketeton

---
+++ script.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function () {
+
+});//////end of document.ready
+
+/****************GLOBAL VARIABLES***************/
+var iWant = {
+    verb: ["read","listen to","watch"]
+};
+////////////randomizeOptions function for randomizing verbs and nouns in landing page
+
