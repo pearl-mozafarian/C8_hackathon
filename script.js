7e7f0d49e3097cbebe04332c360e5af6943ea81c
Erika O'Neal <e.oneal89@gmail.com>
2016-05-12 11:50:02 -0700

    ajax read finished, untested, in wrong spot

--- script.js
+++ script.js
@@ -11,3 +11,19 @@ var iWant = {
 
 
 
+function readAjax() {
+    $.ajax({
+        dataType: 'json',
+        data: {
+            search_term: iWant.selectedNoun,
+        }
+        method: 'post',
+        url: 's-apis.learningfuze.com/hackathon/twitter/index.php',
+        success: function(result) {
+            for (i=0; i < result.statuses; i++){
+                queueArray[i] = result.statuses[i].text;
+            }
+            console.log(queueArray);
+        }
+    })
+}
